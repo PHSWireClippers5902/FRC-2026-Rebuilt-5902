74128ae3f25e890e5c2387482d1d264341beb081
daniel_sab <danielsabalakov@gmail.com>
2026-02-26 14:22:16 -0500

    began adding field oriented commands... probably a post comp goal? Unless I have more time

---
+++ src/main/java/org/frc5902/robot/util/fieldbased/FieldCommand.java
@@ -0,0 +1,51 @@
+/**
+ * IF 
+ * @blame Daniel Sabalakov
+ */
+
+
+package org.frc5902.robot.util.fieldbased;
+
+import org.frc5902.robot.state.RobotState;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.wpilibj2.command.Command;
+import lombok.ToString;
+
+@ToString
+public class FieldCommand extends Command {
+
+    public Debouncer debounceFinish = new Debouncer(0.5, DebounceType.kBoth);
+    public Shape2d shape;
+    public Command whatToRun;
+
+    public FieldCommand(Shape2d encapsulatingShape, Command whatToRun) {
+        this.shape = encapsulatingShape;
+    }
+    // let the command scheduler do its thing
+    @Override
+    public void initialize() {
+        whatToRun.initialize();
+    }
+
+
+    @Override
+    public void execute() {
+        whatToRun.execute();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        whatToRun.end(interrupted);
+    }
+
+
+
+    @Override
+    public boolean isFinished() {
+        return debounceFinish.calculate(
+            shape.insideShape(RobotState.getInstance().getEstimatedPose().getTranslation()));
+    }
+
+}
