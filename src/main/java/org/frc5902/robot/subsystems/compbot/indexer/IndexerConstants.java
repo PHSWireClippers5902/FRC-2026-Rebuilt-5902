f76708912d19d983690fe47e4598bec6160064d3
daniel_sab <danielsabalakov@gmail.com>
2026-03-13 15:54:25 -0400

    implement?

--- src/main/java/org/frc5902/robot/subsystems/compbot/indexer/IndexerConstants.java
+++ src/main/java/org/frc5902/robot/subsystems/compbot/indexer/IndexerConstants.java
@@ -6,15 +6,15 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class IndexerConstants {
     public class IndexConstants {
-        public static int IntakeCANID = -1;
+        public static int IndexCANID = -1;
         public static boolean inverted = true;
         public static final double reduction = 1;
         public static int StallLimit = 70;
         public static int FreeLimit = 45;
-        public static PID IntakePID =
+        public static PID IndexerPID =
                 PID.builder().proportional(0.023).deriviative(0.002).build();
-        public static double IntakePositionConversionFactor = 1.0;
-        public static double IntakeVelocityConversionFactor = IntakePositionConversionFactor / 60;
+        public static double IndexPositionConversionFactor = 1.0;
+        public static double IndexVelocityConversionFactor = IndexPositionConversionFactor / 60;
         public static IdleMode idleMode = IdleMode.kBrake;
     }
 }
--- src/main/java/org/frc5902/robot/subsystems/compbot/indexer/IndexerIOSpark.java
+++ src/main/java/org/frc5902/robot/subsystems/compbot/indexer/IndexerIOSpark.java
@@ -1,5 +1,92 @@
 package org.frc5902.robot.subsystems.compbot.indexer;
 
-public class IndexerIOSpark implements IndexerIO{
-    
+import static org.frc5902.robot.util.motorutil.SparkUtil.tryUntilOk;
+
+import java.util.function.DoubleSupplier;
+
+import org.frc5902.robot.subsystems.compbot.indexer.IndexerConstants.IndexConstants;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.REVLibError;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+
+public class IndexerIOSpark implements IndexerIO {
+    // hardware
+    public final SparkBase Indexer;
+    public final RelativeEncoder IndexerEncoder;
+    public final SparkClosedLoopController IndexerController;
+    // signals
+    public final DoubleSupplier position;
+    public final DoubleSupplier velocity;
+    public final DoubleSupplier appliedVolts;
+    public final DoubleSupplier temp;
+    public final DoubleSupplier current;
+    // outputs
+    public final Debouncer IndexerConnectedDebounce = new Debouncer(0.5, DebounceType.kFalling);
+
+    public IndexerIOSpark() {
+        Indexer = new SparkMax(IndexConstants.IndexCANID, MotorType.kBrushless);
+        var config = new SparkMaxConfig();
+        config.encoder.positionConversionFactor(IndexConstants.IndexPositionConversionFactor);
+        config.encoder.velocityConversionFactor(IndexConstants.IndexVelocityConversionFactor);
+        config.closedLoop.positionWrappingEnabled(false);
+        config.inverted(IndexConstants.inverted)
+                .idleMode(IdleMode.kCoast)
+                .smartCurrentLimit(IndexConstants.StallLimit, IndexConstants.FreeLimit);
+        config.closedLoop.pid(
+                IndexConstants.IndexerPID.getProportional(),
+                IndexConstants.IndexerPID.getIntegral(),
+                IndexConstants.IndexerPID.getDeriviative());
+
+        tryUntilOk(
+                Indexer,
+                5,
+                () -> Indexer.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters));
+        IndexerEncoder = Indexer.getEncoder();
+        tryUntilOk(Indexer, 5, () -> IndexerEncoder.setPosition(0.0));
+        IndexerController = Indexer.getClosedLoopController();
+
+        position = () -> IndexerEncoder.getPosition();
+        velocity = () -> IndexerEncoder.getVelocity();
+        appliedVolts = () -> Indexer.getAppliedOutput();
+        temp = () -> Indexer.getMotorTemperature();
+        current = () -> Indexer.getOutputCurrent();
+    }
+
+    @Override
+    public void updateInputs(IndexerIOInputs inputs) {
+        inputs.data = new IndexerIOData(
+                IndexerConnectedDebounce.calculate(Indexer.getLastError() == REVLibError.kOk),
+                position.getAsDouble(),
+                velocity.getAsDouble(),
+                appliedVolts.getAsDouble(),
+                temp.getAsDouble(),
+                current.getAsDouble());
+    }
+
+    @Override
+    public void runVolts(double volts) {
+        Indexer.setVoltage(volts);
+    }
+
+    @Override
+    public void runRadiansPerSecond(double radiansPerSecond) {
+        IndexerController.setSetpoint(radiansPerSecond, ControlType.kVelocity);
+    }
+
+    @Override
+    public void stop() {
+        Indexer.stopMotor();
+    }   
 }
